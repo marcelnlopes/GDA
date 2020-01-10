ebbf35550c5b6733c0837057765d1c7ba5c9c7e3
marcelnlopes <marcelnlopes@hotmail.com>
2020-01-10 16:58:47 -0300

    Adicionado comportamento relacionado a geração de dezenas para  quina"

--- behaviour.js
+++ behaviour.js
@@ -1,3 +1,24 @@
+function quina()
+{
+    numero = 0;
+    dezenas = [];    
+   
+    document.getElementById('lbl06').style.visibility = 'hidden';
+    document.getElementById('inp06').style.visibility = 'hidden';
+
+    for (i=0; dezenas.length < 5; i++)
+    {
+        while ((dezenas.indexOf(numero) !== -1) || (numero == 0))
+        {
+            numero = Math.floor(Math.random() * (80 - 1 + 1)) + 1;
+            if ((numero.toString().length) === 1) { numero = '0' + numero.toString();}
+        }
+
+        dezenas[i] = numero;
+        document.getElementById('inp0'+ (i + 1).toString() ).value = numero;                
+    }        
+}
+
 function megasena()
 {
     numero = 0;
